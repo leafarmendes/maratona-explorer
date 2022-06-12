162d0fcc49717203854bcbbc1825ef79d1452dfa
Rafael Mendes <rafaelmendesbusi@gmail.com>
2022-06-12 16:35:36 -0300

    Finalizado o projeto. Adicionado lista de perguntas, lógica randomica, adicionei transição com classList.toggle. Consegui sozinho \o/

--- index.js
+++ index.js
@@ -16,3 +16,58 @@ function controlTheme() {
 }
 
 getInputToggle.addEventListener("click", controlTheme);
+
+const answers = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+
+
+const getInputQuestion = document.querySelector('#input-quest');
+const getButtonQuestion = document.querySelector('#make-quest');
+const getAnswerElement = document.querySelector('#answer');
+const classIn = 'in';
+
+
+let myQuestion;
+let myAnswer;
+
+function makeQuestion() {
+  myQuestion = getInputQuestion.value;
+  if(myQuestion == false) {
+    alert('Insira uma pergunta');
+    return;
+  } 
+
+  getAnswerElement.classList.toggle(classIn);
+
+  const totalAnswers = answers.length - 1;
+  const getRandom = Math.floor(Math.random() * totalAnswers);
+  myAnswer = answers[getRandom];
+  
+  getAnswerElement.innerHTML = `${myQuestion} <div>${myAnswer}</div>`;
+
+  setTimeout(function() {
+    getAnswerElement.classList.toggle(classIn);
+  }, 3000)
+}
+
+getButtonQuestion.addEventListener('click', makeQuestion);
